f11522515b52b994f107ea71b79755a0c4a530fa
Santiago Montero <monterosantiago2001@gmail.com>
2022-07-24 22:10:56 -0300

    add scripts

--- app.js
+++ app.js
@@ -1,8 +1,7 @@
 const { app } = require('./routes/routes.js')
+const  config  = require('../config')
 
-
-
-const PORT = process.env.PORT
+const PORT = config.PORT
 
 app.listen(PORT, () =>{
     console.log(`Server is runnig in port ${PORT}`)
--- config.js
+++ config.js
@@ -3,4 +3,5 @@ require('dotenv').config()
 module.exports = {
     email_admin: process.env.EMAIL_ADMIN,
     password_admin:  process.env.PASSWORD_ADMIN,
+    PORT : process.env.PORT
 }
