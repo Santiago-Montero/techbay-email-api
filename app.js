7294ab78b7c050f9d35a9fb43c392e26a25e8b1d
Santiago Montero <monterosantiago2001@gmail.com>
2022-07-25 23:07:18 -0300

    Add cors

--- app.js
+++ app.js
@@ -4,12 +4,12 @@ const cors=require("cors");
 
 const PORT = config.PORT
 
-//app.use(cors())
+app.use(cors())
 
 app.use((req, res, next) => {
-    res.header('Access-Control-Allow-Origin', '*');
-    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
-    res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization, Content-Length, X-Requested-With');
+    res.append('Access-Control-Allow-Origin', ['*']);
+    res.append('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
+    res.append('Access-Control-Allow-Headers', 'Content-Type');
     next();
 });
 
