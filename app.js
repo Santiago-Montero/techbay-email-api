2b3eb1c8e008564cacafde4b57f9f218bcd45391
Santiago Montero <monterosantiago2001@gmail.com>
2022-07-25 21:10:06 -0300

    add cors

--- app.js
+++ app.js
@@ -1,7 +1,16 @@
 const { app } = require('./routes/routes.js')
 const  config  = require('./config')
+const cors=require("cors");
 
 const PORT = config.PORT
+
+const corsOptions ={
+   origin:'*', 
+   credentials:true,            //access-control-allow-credentials:true
+   optionSuccessStatus:200,
+}
+
+app.use(cors(corsOptions)) // Use this after the variable declaration
 
 app.listen(PORT, () =>{
     console.log(`Server is runnig in port ${PORT}`)
