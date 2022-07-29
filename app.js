ac24657b076eb22c8e495d25a2e73eae1fcd6aaa
Santiago Montero <monterosantiago2001@gmail.com>
2022-07-29 19:30:39 -0300

    cors

--- app.js
+++ app.js
@@ -4,15 +4,6 @@ const cors=require("cors");
 
 const PORT = config.PORT
 
-app.use(cors())
-
-app.use((req, res, next) => {
-    res.append('Access-Control-Allow-Origin', '*');
-    res.append('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
-    res.append('Access-Control-Allow-Headers', 'Content-Type');
-    next();
-});
-
 app.listen(PORT, () =>{
     console.log(`Server is runnig in port ${PORT}`)
 });
